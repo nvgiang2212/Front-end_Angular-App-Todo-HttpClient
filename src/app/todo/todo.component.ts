2202f26ba65fde3c317ddb13199d4eed9e57a2c2
vgiang <Giang221295>
2019-08-13 01:21:20 +0700

    add component todo

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoService } from '../todo.service';
+import { ITodo } from '../todo';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-todo',
@@ -6,10 +9,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo.component.scss']
 })
 export class TodoComponent implements OnInit {
-
-  constructor() { }
+  todoList: ITodo[] = [];
+  inputControl = new FormControl();
+  constructor(private todoService: TodoService) { }
 
   ngOnInit() {
+    this.todoService.getTodos().subscribe(next => {
+      this.todoList = next;
+    }, error => {
+      console.log(error);
+    }, () => {
+      console.log('complete');
+    });
+  }
+
+  toggleTodo(i) {
+    const todo = this.todoList[i];
+    const todoData = {
+      ...todo,
+      completed: !todo.completed
+    };
+    this.todoService.updateTodo(todoData).subscribe(next => {
+      this.todoList[i].completed = next.completed;
+    });
+  }
+
+  addTodo() {
+    const todo: Partial<ITodo> = {
+      title: this.inputControl.value,
+      completed: false
+    };
+
+    this.todoService.createTodo(todo).subscribe(next => {
+      this.todoList.unshift(next);
+      this.inputControl.setValue('');
+    });
+  }
+
+  deleteTodo(i) {
+    const todo = this.todoList[i];
+    this.todoService.deleteTodo(todo.id).subscribe(() => {
+      this.todoList = this.todoList.filter(t => t.id !== todo.id);
+    });
   }
 
 }
