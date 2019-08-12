9ae85f2a280e7d57112bac44a7a4cd87ffee8161
vgiang <Giang221295>
2019-08-13 01:04:54 +0700

    add post.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
+import { BlogComponent } from './blog/blog.component';
+import { BlogDetailComponent } from './blog-detail/blog-detail.component';
+import { BlogEditComponent } from './blog-edit/blog-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    BlogComponent,
+    BlogDetailComponent,
+    BlogEditComponent
   ],
   imports: [
     BrowserModule,
