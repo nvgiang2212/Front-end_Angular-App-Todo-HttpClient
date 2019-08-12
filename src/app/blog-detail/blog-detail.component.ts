92d28df76132b47a182f6d63c9543408bcfd92bf
vgiang <Giang221295>
2019-08-13 01:34:00 +0700

    add blog-detail component

--- src/app/blog-detail/blog-detail.component.ts
+++ src/app/blog-detail/blog-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PostService } from '../post.service';
+import { IPost } from '../post';
 
 @Component({
   selector: 'app-blog-detail',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogDetailComponent implements OnInit {
 
-  constructor() { }
+  post: IPost;
+  constructor(
+    private route: ActivatedRoute,
+    private postService: PostService
+  ) {}
 
   ngOnInit() {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.postService.getPostById(id).subscribe(
+      next => (this.post = next),
+      error => {
+        console.log(error);
+        this.post = null;
+      }
+    );
   }
 
 }
