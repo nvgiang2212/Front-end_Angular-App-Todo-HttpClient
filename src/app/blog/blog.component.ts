1d785e22ede27cbde4634f7501de0fd6fb69e8c4
vgiang <Giang221295>
2019-08-13 01:36:41 +0700

    add blog component

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from '../post.service';
+import { IPost } from '../post';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-blog',
@@ -7,9 +10,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogComponent implements OnInit {
 
-  constructor() { }
+  postList: IPost[] = [];
+  postForm: FormGroup;
+  constructor(
+    private postService: PostService,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit() {
+    this.postForm = this.fb.group({
+      title: ['', [Validators.required, Validators.minLength(10)]],
+      body: ['', [Validators.required, Validators.minLength(10)]],
+    });
+    this.postService
+      .getPosts()
+      .subscribe(next => (this.postList = next), error => (this.postList = []));
+  }
+
+  onSubmit() {
+    if (this.postForm.valid) {
+      const {value} = this.postForm;
+      this.postService.createPost(value)
+        .subscribe(next => {
+          this.postList.unshift(next);
+          this.postForm.reset({
+            title: '',
+            body: ''
+          });
+        }, error => console.log(error));
+    }
+  }
+
+  deletePost(i) {
+    const post = this.postList[i];
+    this.postService.deletePost(post.id).subscribe(() => {
+      this.postList = this.postList.filter(t => t.id !== post.id);
+    });
   }
 
 }
